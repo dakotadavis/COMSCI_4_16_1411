3b44d8376ac46e8848cb0baad0d2aab6a8aab7b0
Dakota Davis <davisds@student.swosu.edu>
2015-11-04 09:32:07 -0600

    Working character generation

--- src/COMSCI_4_16_1411.java
+++ src/COMSCI_4_16_1411.java
@@ -1,6 +1,15 @@
 public class COMSCI_4_16_1411 {
 	public static void main(String[] args) {
-		System.out.println("")
+		
+		
+		// A=65, Z=90
+		// We want to go from 65 and 65 + 25
+		// a will be 65
+		// b will be 25
+		int number = (int) (65 + Math.random() *(25+1));
+		System.out.println (number);
+		char letter =(char)number;
+		System.out.println (letter);
 
 	}
 
