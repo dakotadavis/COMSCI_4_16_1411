fff8fce2bfdf2643194de75f38391cda41222f85
Dakota Davis <davisds@student.swosu.edu>
2015-11-04 09:49:04 -0600

    simplified to a single line

--- src/COMSCI_4_16_1411.java
+++ src/COMSCI_4_16_1411.java
@@ -1,15 +1,31 @@
 public class COMSCI_4_16_1411 {
 	public static void main(String[] args) {
 		
+		int startingValue = 65;
+		int endingValue = 90 - 65 + 1;
+		/* Math.random excludes the upper limit, so we must add the 1 to include
+		 * the upper limit of Z 
+		 */
+		
+		// For lowercase letters:
+		// int startingValue = 97;
+		// int endingValue = 122 - 97+ 1;
 		
 		// A=65, Z=90
 		// We want to go from 65 and 65 + 25
 		// a will be 65
 		// b will be 25
-		int number = (int) (65 + Math.random() *(25+1));
-		System.out.println (number);
-		char letter =(char)number;
-		System.out.println (letter);
+		
+		// Old code:
+		// int number = (int) (startingValue + Math.random() *(endingValue));
+		// System.out.println (number);
+		// char letter =(char)number;
+		// System.out.println (letter);
+		
+		// To simplify:
+		System.out.println((char)(startingValue + Math.random()*(endingValue)));
+		
+		
 
 	}
 
